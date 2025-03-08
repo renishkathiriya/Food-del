8a578f178cbaf5bc509af371f001288368b382f9
renishkathiriya <renishkathiriya55@gmail.com>
2025-03-08 12:31:12 +0530

    addmin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Sidebar from './components/Sidebar/Sidebar'
+import { Route, Routes } from 'react-router-dom'
+import List from './pages/List/List'
+import Orders from './pages/Orders/Orders'
+import Add from './pages/Add/Add'
 
 const App = () => {
   return (
     <div>
-      
+      <Navbar />
+      <hr />
+      <div className="app-conten">
+        <Sidebar/>
+         <Routes>
+          <Route path='/add' element = {<Add/>} />
+          <Route path='/list' element = {<List/>} />
+          <Route path='/orders' element = {<Orders/>} />
+         </Routes>
+      </div>
     </div>
   )
 }
