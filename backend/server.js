44f17b18a01225f22e37345d9be4b039c059e82c
renishkathiriya <renishkathiriya55@gmail.com>
2025-03-19 17:48:26 +0530

    order

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
+import dotenv from "dotenv"
 import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
-import 'dotenv/config'
 import bodyParser from "body-parser"
 import cartRouter from "./routes/cartRoute.js"
-
-
+import orderRouter from "./routes/orderRoute.js"
+dotenv.config();
 // app Config
 const app = express()
 const port = 4000
@@ -21,9 +21,11 @@ connectDB();
 
 // api endpoints
 app.use("/api/food",foodRouter)
-app.use("/images",express.static('uploads'))
+app.use("/images",express.static('uploads')) 
 app.use("/api/user",userRouter)
 app.use("/api/cart",cartRouter)
+app.use("/api/order",orderRouter);
+
 
 
 app.get("/",(req,res)=>{
