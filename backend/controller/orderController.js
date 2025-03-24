f19f8bdfb2e417f56d2328880362021d1048a39c
renishkathiriya <renishkathiriya55@gmail.com>
2025-03-24 16:02:14 +0530

    adminorder

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -88,4 +88,18 @@ const userOrders = async (req, res) => {
       }
 }
 
-export { placeOrder, verifyOrder, userOrders }
+
+// LISTING ORDERS FOR ADMiN PANEL
+
+const listOrders = async (req,res) => {
+      try {
+            const orders = await orderModel.find({});
+            res.json({success:true,data:orders})
+      } catch (error) {
+            console.log(error);
+            res.json({success:false,message:"ERROR"})
+      }
+}
+
+
+export { placeOrder, verifyOrder, userOrders,listOrders }
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import authMiddleware from '../middleware/auth.js';
-import { placeOrder, userOrders, verifyOrder } from '../controller/orderController.js'
+import { placeOrder, userOrders, verifyOrder,listOrders } from '../controller/orderController.js'
 const orderRouter = express.Router();
 
 orderRouter
       .post("/place", authMiddleware, placeOrder)
       .post("/verify", verifyOrder)
       .post("/userorders", authMiddleware, userOrders)
+      .get("/list",listOrders)
 
 export default orderRouter;
